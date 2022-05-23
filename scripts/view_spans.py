a762c4e53de139939245dcdd6c52a75595df74dc
Lj Miranda <ljvmiranda@gmail.com>
2022-05-23 14:47:55 +0800

    Add view_spans CLI to easily inspect saved DocBins

---
+++ scripts/view_spans.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Optional
+
+import spacy
+import typer
+from spacy.tokens import DocBin
+from spacy.util import get_lang_class
+from wasabi import msg
+
+Arg = typer.Argument
+Opt = typer.Option
+
+
+def view_spans(
+    # fmt: off
+    input_path: Path = Arg(..., help="Input spaCy file", exists=True),
+    lang: str = Opt("xx", "--lang", "-l", help="Language for the vocab"),
+    display_size: int = Opt(3, "--size", "-s", help="Number of Doc.spans to show"),
+    # fmt: on
+):
+    """Helper function to check if spans were saved correctly"""
+    nlp = get_lang_class(lang)()
+    db = DocBin().from_disk(input_path)
+    docs = list(db.get_docs(nlp.vocab))
+    msg.info(f"Found {len(docs)} docs in {str(input_path)}")
+    for doc in docs[:display_size]:
+        msg.text(doc.spans)
+
+
+if __name__ == "__main__":
+    typer.run(view_spans)
