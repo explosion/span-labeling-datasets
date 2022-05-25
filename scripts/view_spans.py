3f56a9730a80011267793668f648fac7a12765d6
Lj Miranda <ljvmiranda@gmail.com>
2022-05-25 09:08:00 +0800

    Add shuffle parameter to view_spans

--- scripts/view_spans.py
+++ scripts/view_spans.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import typer
@@ -13,7 +14,8 @@ def view_spans(
     # fmt: off
     input_path: Path = Arg(..., help="Input spaCy file", exists=True),
     lang: str = Opt("xx", "--lang", "-l", help="Language for the vocab"),
-    display_size: int = Opt(3, "--size", "-s", help="Number of Doc.spans to show"),
+    display_size: int = Opt(3, "--size", "-sz", help="Number of Doc.spans to show"),
+    shuffle: bool = Opt(False, "--shuffle", "-s", help="Shuffle the items")
     # fmt: on
 ):
     """Helper function to check if spans were saved correctly"""
@@ -22,6 +24,9 @@ def view_spans(
     docs = list(db.get_docs(nlp.vocab))
     msg.info(f"Found {len(docs)} docs in {str(input_path)}")
     msg.info(f"Showing {display_size} docs")
+
+    if shuffle:
+        random.shuffle(docs)
     for doc in docs[:display_size]:
         msg.divider()
         msg.text(doc.text)
