db28c2cf82f38511bda4458ebc42e6db13f631e3
Lj Miranda <ljvmiranda@gmail.com>
2022-06-17 07:42:51 +0800

    [wip] Update

--- scripts/split_docs.py
+++ scripts/split_docs.py
@@ -1,6 +1,7 @@
 """Split a spaCy-formatted file into train, dev, and test partitions"""
 
-from pathlib import Path, Tuple
+from pathlib import Path
+from typing import Tuple
 
 import typer
 from wasabi import msg
@@ -16,4 +17,13 @@ def split_docs(
     split_size: Tuple[float, float, float] = Arg((0.8, 0.1, 0.1), help="Split sizes for train/dev/test respectively"),
     # fmt: on
 ):
-    pass
+    if sum(split_size) != 1.0:
+        msg.fail(
+            "Split sizes for train, dev, and test should sum up to 1.0 "
+            f"({' + '.join(map(str, split_size))} != 1.0)",
+            exits=1,
+        )
+
+
+if __name__ == "__main__":
+    typer.run(split_docs)
