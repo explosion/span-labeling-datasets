3220192ed0b0ac4c2453ba42a4e950b01bd28b7f
kadarakos <kadar.akos@gmail.com>
2022-06-29 15:06:21 +0000

    formatting

--- scripts/char_lstm.py
+++ scripts/char_lstm.py
@@ -10,9 +10,7 @@ from thinc.api import with_getitem, with_array, with_padded
 from thinc.api import get_array_module
 
 
-def docs2unicode() -> Model[List[Doc], Tuple[
-        List[Ints1d], List[Ints1d], List[Ints1d]
-]]:
+def docs2unicode() -> Model[List[Doc], Tuple[List[Ints1d], List[Ints1d], List[Ints1d]]]:
     """
     Convert a list of docs into variable length unicode arrays.
     The layer also returns the start and end indices for each token.
@@ -30,18 +28,15 @@ def gather_tokens() -> Model:
     return Model("gather_tokens", forward_gather_tokens)
 
 
-def to_unicode(
-    doc: Doc,
-    xp
-) -> Tuple[Ints1d, Ints1d, Ints1d]:
+def to_unicode(doc: Doc, xp) -> Tuple[Ints1d, Ints1d, Ints1d]:
     """
     Convert Doc into a list of unicode codepoints
     and also return the word-boundaries.
     Adds white space " " between each token to signal
     word-boundaries.
     """
-    token_starts = xp.zeros(len(doc), dtype='int')
-    token_ends = xp.zeros(len(doc), dtype='int')
+    token_starts = xp.zeros(len(doc), dtype="int")
+    token_ends = xp.zeros(len(doc), dtype="int")
     byte_list = []
     for i, token in enumerate(doc):
         token_codepoint = [ord(char) for char in token.orth_ + " "]
@@ -55,9 +50,7 @@ def to_unicode(
 
 
 def forward_docs2unicode(
-    model: Model,
-    docs: List[Doc],
-    is_train: bool
+    model: Model, docs: List[Doc], is_train: bool
 ) -> Tuple[List[Ints1d], List[Ints1d], List[Ints1d]]:
     byte_list, start_list, end_list = [], [], []
     xp = model.ops.xp
@@ -76,13 +69,13 @@ def forward_docs2unicode(
 def forward_gather_tokens(
     model: Model,
     features_and_bounds: Tuple[List[Floats2d], List[Ints1d], List[Ints1d]],
-    is_train: bool
+    is_train: bool,
 ) -> List[Floats2d]:
     bilstm_features, start_inds, end_inds = features_and_bounds
     assert len(bilstm_features) == len(start_inds)
     assert len(start_inds) == len(end_inds)
-    
-    def backprop(dY: List[Floats2d]) -> Tuple[List[Floats2d], List] :
+
+    def backprop(dY: List[Floats2d]) -> Tuple[List[Floats2d], List]:
         l_dX = []
         for i, dy in enumerate(dY):
             dX = xp.zeros_like(bilstm_features[i])
@@ -91,17 +84,12 @@ def forward_gather_tokens(
             dX[end_inds[i]] = d_end
             l_dX.append(dX)
         return l_dX, []
-    
-    tokens_features = []
-    for char_feats, tok_starts, tok_ends in zip(
-            bilstm_features, start_inds, end_inds
-    ):
-        xp = get_array_module(char_feats)
-        token_features = xp.hstack(
-            (char_feats[tok_starts], char_feats[tok_ends])
-        )
-        tokens_features.append(token_features)
 
+    tokens_features = []
+    for char_feats, tok_starts, tok_ends in zip(bilstm_features, start_inds, end_inds):
+        xp = get_array_module(char_feats)
+        token_features = xp.hstack((char_feats[tok_starts], char_feats[tok_ends]))
+        tokens_features.append(token_features)
 
     return tokens_features, backprop
 
@@ -129,26 +117,16 @@ def UnicodeLSTMEmbed(
     forward and backward LSTM states at the start and the end of the token.
 
     """
-    embedder = HashEmbed(
-        width, rows, seed=seed
-    )
+    embedder = HashEmbed(width, rows, seed=seed)
     bilstm_ = spacy.registry.architectures.get("spacy.TorchBiLSTMEncoder.v1")
     norm = thinc.registry.layers.get("LayerNorm.v1")
     relu = thinc.registry.layers.get("Relu.v1")
     bilstm = bilstm_(width, depth, dropout)
-    mlp_out = chain(
-        relu(width, normalize=True),
-        relu(width, normalize=True)
-    )
+    mlp_out = chain(relu(width, normalize=True), relu(width, normalize=True))
     encode = chain(
         docs2unicode(),
         with_getitem(
-            0,
-            chain(
-                with_array(embedder),
-                with_padded(bilstm),
-                with_array(mlp_out)
-            )
+            0, chain(with_array(embedder), with_padded(bilstm), with_array(mlp_out))
         ),
         gather_tokens(),
     )
