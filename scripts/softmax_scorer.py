114cc759a1746d812d959668d533d20faf4503d2
Lj Miranda <ljvmiranda@gmail.com>
2022-07-06 12:49:25 +0800

    Add custom script that registers custom function

---
+++ scripts/softmax_scorer.py
@@ -0,0 +1,11 @@
+"""Softmax scorer for exclusive classes in spancat"""
+
+from spacy import registry
+from thinc.api import Softmax, Model, glorot_uniform_init
+from thinc.types import Floats2d
+
+
+@registry.layers("spacy.Softmax.v1")
+def build_softmax(nO=None, nI=None) -> Model[Floats2d, Floats2d]:
+    """An output layer for span classification. Uses a softmax layer for exclusive classes"""
+    return Softmax(nO=nO, nI=nI, init_w=glorot_uniform_init)
