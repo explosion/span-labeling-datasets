d7123e7e0c08f8c005cd7b2f406134240175eb9b
Lj Miranda <ljvmiranda@gmail.com>
2022-07-07 12:20:51 +0800

    Fix wrong parameter

--- scripts/custom_components/softmax_scorer.py
+++ scripts/custom_components/softmax_scorer.py
@@ -8,4 +8,4 @@ from thinc.types import Floats2d
 @registry.layers("spacy.Softmax.v1")
 def build_softmax(nO=None, nI=None) -> Model[Floats2d, Floats2d]:
     """An output layer for span classification. Uses a softmax layer for exclusive classes"""
-    return Softmax(nO=nO, nI=nI, init_w=glorot_uniform_init)
+    return Softmax(nO=nO, nI=nI, init_W=glorot_uniform_init)
