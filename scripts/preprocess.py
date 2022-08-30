c37cbe237dc9dbd9e6902f6e0f182770a874ee93
kadarakos <kadar.akos@gmail.com>
2022-08-30 17:50:12 +0000

    use original data sources

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -14,10 +14,6 @@ def preprocess(input_path: Path, output_path: Path):
         lines = f.readlines()
     with output_path.open("w") as f:
         for i, line in enumerate(lines):
-            # XXX bit weird, but line 9883 in the German ConLL dev set
-            # seems to be broken: it only has field O, but no string.
-            if i == 9883 and input_path.name == "raw-de-conll-test.iob":
-                continue
             new_line = line if line == "\n" else line.lstrip(digits)[1:]
             if new_line == "-DOCSTART-\tO\n":
                 new_line = "-DOCSTART- -X- O O\n"
