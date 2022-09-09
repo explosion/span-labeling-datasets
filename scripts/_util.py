97409eb0768be469ff20a92db2efad478f35fea8
kadarakos <kadar.akos@gmail.com>
2022-09-09 19:34:25 +0000

    analysis script

---
+++ scripts/_util.py
@@ -0,0 +1,79 @@
+import os
+from pathlib import Path
+from typing import Union
+from collections import defaultdict
+from spacy.util import ensure_path
+from dataclasses import dataclass
+
+
+@dataclass
+class SplitInfo:
+    """
+    Provides convenient wrapper to parse
+    the data file names, but its also useful
+    to validate that the file names are in
+    stardardized format.
+    """
+    path: Union[Path, str]
+
+    def __post_init__(self):
+        self.path = ensure_path(self.path)
+        self.name = self.path.name
+        tokens = self.name.split("-")
+        if not 1 < len(tokens) <= 3:
+            raise ValueError(
+                f"Incorrect file name {self.name}"
+            )
+        self.source = tokens[0]
+        self.split = tokens[-1].split(".")[0]
+        if self.split not in {"train", "dev", "test"}:
+            raise ValueError(
+                "Splits has to be either 'train', 'dev' or 'test', "
+                f"but found {self.split}"
+            )
+        if len(tokens) == 3:
+            self.lang = tokens[1]
+        else:
+            self.lang = ""
+
+
+@dataclass
+class DatasetInfo:
+    train: SplitInfo
+    dev: SplitInfo
+    test: SplitInfo
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+
+def info(model: str, *, home: str = "corpus"):
+    """
+    Provides convenient wrapper to avoid keep
+    parsing the filenames. Its also useful to
+    validate that all splits are there and the
+    filenames are in the standardized format.
+    """
+    assert model in ["ner", "spancat"]
+    home = os.path.join(home, model)
+    filenames = os.listdir(home)
+    splits = []
+    for name in filenames:
+        path = os.path.join(home, name)
+        splits.append(SplitInfo(path))
+    datasets = defaultdict(dict)
+    for split in splits:
+        datasets[split.source][split.split] = split
+    out = {}
+    for source in datasets:
+        if len(datasets[source]) < 3:
+            raise ValueError(
+                "Each dataset has to have 3 splits"
+            )
+        datainfo = DatasetInfo(
+            datasets[source]["train"],
+            datasets[source]["dev"],
+            datasets[source]["test"]
+        )
+        out[source] = datainfo
+    return out
---
+++ scripts/analyze.py
@@ -0,0 +1,90 @@
+import os
+import csv
+
+import spacy
+import typer
+
+from pathlib import Path
+from spacy.tokens import Doc, DocBin
+from collections import defaultdict
+from typing import Sequence
+
+from _util import info
+
+
+def _per_label_fields():
+    return {"count": 0, "length": 0, "doc_freq": 0}
+
+
+def _per_label(docs: DocBin):
+    classes = defaultdict(_per_label_fields)
+    for i, doc in enumerate(docs, start=1):
+        seen = set()
+        for span in doc.spans['sc']:
+            label = span.label_
+            classes[label]['count'] += 1
+            classes[label]["length"] += len(span)
+            if label not in seen:
+                classes[label]["doc_freq"] += 1
+                seen.add(label)
+    for key in classes:
+        classes[key]["length"] /= classes[key]["count"]
+    return classes
+
+
+def _per_span(docs: Sequence[Doc]):
+    spans = []
+    for doc in docs:
+        for span in doc.spans["sc"]:
+            row = [len(span), len(doc), len(doc.spans["sc"])]
+            spans.append(row)
+    return spans
+
+
+def analyze(
+    dataset: str,
+    model: str,
+    split: str,
+    *,
+    data_dir: str = "corpus",
+    output_dir: str = "analytics"
+):
+    datainfo = info("spancat", home=data_dir)
+    path = datainfo[dataset][split].path
+    nlp = spacy.load(model)
+    docs = list(
+        DocBin().from_disk(path).get_docs(nlp.vocab)
+    )
+    label_stats = _per_label(docs)
+    span_stats = _per_span(docs)
+    label_stats_path = os.path.join(
+        output_dir, f"{dataset}-{split}-labels.csv"
+    )
+    span_stats_path = os.path.join(
+        output_dir, f"{dataset}-{split}-spans.csv"
+    )
+    with open(label_stats_path, "w") as csvfile:
+        fieldnames = ["label", "count", "length", "doc_freq"]
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames
+        )
+        for label, stats in label_stats.items():
+            row = {"label": label}
+            row.update(stats)
+            writer.writerow(row)
+    with open(span_stats_path, "w") as csvfile:
+        fieldnames = ["length", "doc_length", "neighbours"]
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames
+        )
+        for record in span_stats:
+            row = {
+                "length": record[0],
+                "doc_length": record[1],
+                "neighbours": record[2]
+            }
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    typer.run(analyze)
