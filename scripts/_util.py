a6abd8b8201ba51d3c72ffeaca9d72b50e7c6c4b
kadarakos <kadar.akos@gmail.com>
2022-09-27 12:40:05 +0000

    clarifying comment

--- scripts/_util.py
+++ scripts/_util.py
@@ -45,6 +45,7 @@ class SplitInfo:
             source = tokens[1]
             self.lang = tokens[0]
             self.source = f"{self.lang}-{source}"
+        # known to be English data sets.
         elif tokens[0] in ["anem", "wnut17", "archaeo", "finer"]:
             source = tokens[0]
             self.source = tokens[0]
